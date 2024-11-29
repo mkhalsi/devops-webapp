3f457f88bfea27a2961becb335b495463ca01a22
mkhalsi <khmariam.m@gmail.com>
2024-11-29 19:57:47 +0100

    test deployy

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ date_default_timezone_set('America/New_York'); // Changez selon votre fuseau hor
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1> Bienvenue à mon Web App ! </h1> 
+    <h1> modification! </h1> 
     <div class="container">
         <p id="currDate"><?php echo "Today's Date is: " . date("m/d/Y"); ?></p>
         <p>Enter your Date of Birth in format: (MM/DD/YYYY)</p>
